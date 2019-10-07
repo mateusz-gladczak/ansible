5a8b6ad0647c13881ae3d518113645eb3ea3a6f1
Mateusz Gładczak <mateusz@gladczak.com>
2019-10-07 21:02:50 +0200

    added powershell script

---
+++ files/create_bginfo_shortcut.ps1
@@ -0,0 +1,5 @@
+﻿$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\run_bginfo.lnk")
+$Shortcut.TargetPath = "C:\Windows\bginfo.exe"
+$Shortcut.Arguments = " c:\windows\ansible.bgi /timer:0 /silent /NOLICPROMPT"
+$Shortcut.Save()
