aebf6f085a4ee426246d10945b47dabe6b8b6266
mateusz-gladczak <mateusz@gladczak.com>
2019-11-15 10:31:36 +0100

    Update install-ansible-debian.sh

--- install-ansible-debian.sh
+++ install-ansible-debian.sh
@@ -6,41 +6,41 @@ apt install ansible python3-pip python-dev libkrb5-dev krb5-user
 pip3 install pywinrm[kerberos]
 pip3 install pykerberos --upgrade
 pip3 install requests-kerberos --upgrade
-echo ""
-echo ""
-echo "-----------------------------------------------------"
-echo "configure /etc/krb5.conf for kerberos authentication"
-echo "-----------------------------------------------------"
-echo ""
-echo ""
-echo ""
-echo "        default_realm = NWTRADERS.MSFT"
-echo ""
-echo "[realms]"
-echo "        NWTRADERS.MSFT = {"
-echo "                kdc = DC1.NWTRADERS.MSFT"
-echo "                admin_server = DC1.NWTRADERS.MSFT"
-echo "        }"
-echo "        TALSPINTOYS.COM = {"
-echo "                kdc = DC1.TALSPINTOYS.COM"
-echo "                admin_server = DC1.TALSPINTOYS.COM"
-echo "       }"
-echo ""
-echo "[domain_realm]"
-echo "        .nwtraders.msft = NWTRADERS.MSFT"
-echo "        nwtraders.msft = NWTRADERS.MSFT"
-echo "        .talspintoys.com = TALSPINTOYS.COM"
-echo "        talspintoys.com = TALSPINTOYS.COM"
-echo ""
-echo "-----------------------------------------------------"
-echo "Initialilze kerberos ticket:"
-echo "kinit user@<DOMAIN_REALM>"
-echo ""
-echo "-----------------------------------------------------"
-echo "Use variables in ansible hosts files:"
-echo ""
-echo "[<hosts group>:vars]"
-echo "ansible_user = user@<DOMAIN_REALM>"
-echo "ansible_connection = winrm"
-echo "ansible_port = 5985"
-echo "ansible_winrm_transport = kerberos"
+
+cat << EOF
+
+-----------------------------------------------------
+configure /etc/krb5.conf for kerberos authentication
+-----------------------------------------------------
+
+        default_realm = NWTRADERS.MSFT
+
+[realms]
+        NWTRADERS.MSFT = {
+                kdc = DC1.NWTRADERS.MSFT
+                admin_server = DC1.NWTRADERS.MSFT
+        }
+        TALSPINTOYS.COM = {
+                kdc = DC1.TALSPINTOYS.COM
+                admin_server = DC1.TALSPINTOYS.COM
+       }
+
+[domain_realm]
+        .nwtraders.msft = NWTRADERS.MSFT
+        nwtraders.msft = NWTRADERS.MSFT
+        .talspintoys.com = TALSPINTOYS.COM
+        talspintoys.com = TALSPINTOYS.COM
+
+-----------------------------------------------------
+Initialilze kerberos ticket:
+kinit user@<DOMAIN_REALM>
+
+-----------------------------------------------------
+Use variables in ansible hosts files:
+
+[<hosts group>:vars]
+ansible_user = user@<DOMAIN_REALM>
+ansible_connection = winrm
+ansible_port = 5985
+ansible_winrm_transport = kerberos
+EOF
